2c3343a0615e7ce0314b0b2591e2083c08b810de
bemuse01 <sequeland@gmail.com>
2022-08-01 11:36:51 +0900

    update

--- assets/js/class/test/build/test.child.build.js
+++ assets/js/class/test/build/test.child.build.js
@@ -92,9 +92,13 @@ export default class{
             texture,
             shader: Shader.position,
             uniforms: {
-                
+                time: {value: 0},
+                res: {value: new THREE.Vector2(this.size.obj.w, this.size.obj.h)},
+                rad: {value: this.radius}
             }
         })
+
+        console.log(this.size.obj)
     }
     fillPositionTexture(texture, {obj}){
         const {data, width, height} = texture.image
@@ -127,6 +131,10 @@ export default class{
     animate(){
         this.gpuCompute.compute()
 
+        const time = window.performance.now()
+
         this.circle.setUniform('tPosition', this.gpuCompute.getCurrentRenderTarget(this.positionVariable.get()).texture)
+
+        this.positionVariable.setUniform('time', time)
     }
 }
--- assets/js/class/test/shader/test.child.shader.js
+++ assets/js/class/test/shader/test.child.shader.js
@@ -27,12 +27,27 @@ export default {
         }
     `,
     position: `
+        uniform float time;
+        uniform vec2 res;
+        uniform float rad;
+
+        ${ShaderMethod.rand()}
+
         void main(){
             vec2 uv = gl_FragCoord.xy / resolution.xy;
             ivec2 coord = ivec2(gl_FragCoord.xy);
-            ivec2 res = ivec2(resolution.xy);
-            
+            // ivec2 res = ivec2(resolution.xy);
+
+            // x: particle position x
+            // y: particle position y
             vec4 pos = texture(tPosition, uv);
+
+            pos.y -= 0.1;
+
+            if(pos.y < -res.y * 0.5 - rad){
+                pos.x = rand(vec2(time * 0.001 * uv.x, time * 0.01)) * res.x - (res.x * 0.5);
+                pos.y = rand(vec2(time * 0.002 * uv.y, time * 0.02)) * res.y - (res.y * 0.5);
+            }
 
             gl_FragColor = pos;
         }
