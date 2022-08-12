35f4b9268d11d1ad05225b559759a93c9e2f0907
bemuse01 <sequeland@gmail.com>
2022-08-12 14:05:50 +0900

    Update test.child.build.js

--- assets/js/class/test/build/test.child.build.js
+++ assets/js/class/test/build/test.child.build.js
@@ -361,23 +361,6 @@ export default class{
             const textures = this.sources.map(file => loader.load(file))
         })
     }
-    random(from=null,to=null,interpolation=null){
-        if(from==null){
-          from=0;
-          to=1;
-        }else if(from!=null && to==null){
-          to=from;
-          from=0;
-        }
-        const delta=to-from;
-      
-        if(interpolation==null){
-          interpolation=(n)=>{
-            return n;
-          }
-        }
-        return from+(interpolation(Math.random())*delta);
-    }
 
 
     // animate
